0530aa3886a907068a7c6f930c4788e489ee8b1b
EligibleDev <eligibledev@gmail.com>
2024-03-27 00:01:19 +0600

    27th march

---
+++ class1/variables.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var userName = "Mikail";
+var userId = 54651656;
+var isUserVerified = false;
+var user = { userName: userName, userId: userId, isUserVerified: isUserVerified };
+console.log(user);
--- class1/variables.ts
+++ class1/variables.ts
@@ -1,1 +1,9 @@
-console.log("first line of typescript code");
+let userName: string = "Mikail";
+let userId: number = 54651656;
+let isUserVerified: boolean = false;
+
+const user: object = { userName, userId, isUserVerified };
+
+console.log(user);
+
+export {};
