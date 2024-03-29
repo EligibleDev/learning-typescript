517fac578eb9ac5ef62d99cfd3e4d35208354124
EligibleDev <eligibledev@gmail.com>
2024-03-30 00:12:17 +0600

    class3 will be continued

---
+++ class3/functions.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var addTwo = function (int) {
+    //     return int + 2;
+    return int.toUpperCase();
+};
+var myInt = addTwo(5);
+console.log(myInt);
---
+++ class3/functions.ts
@@ -0,0 +1,8 @@
+const addTwo = (int: number) => {
+    return int + 2;
+};
+
+const myInt = addTwo(5);
+console.log(myInt);
+
+export {};
